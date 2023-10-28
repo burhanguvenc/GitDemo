05e69e0b663aadd05f40331264eeaac6c3e51125
Burhan Guvenc <crosshawk_21@hotmail.com>
2023-10-28 06:08:29 -0400

    Final changes!

--- src/GitContBurhan.java
+++ src/GitContBurhan.java
@@ -1,3 +1,5 @@
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,6 +13,8 @@ public class GitContBurhan {
 		System.out.println("What up?");
 		
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("google.com");
 		driver.findElement(By.id("serch"));
 		driver.close();
