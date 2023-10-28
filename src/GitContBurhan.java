2d7f9eda704952a793ac257b3291ef4231c79757
Burhan Guvenc <crosshawk_21@hotmail.com>
2023-10-28 05:24:17 -0400

    burhans second commit

--- src/GitContBurhan.java
+++ src/GitContBurhan.java
@@ -4,6 +4,7 @@ public class GitContBurhan {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Burhan's work");
+		System.out.println("Burhan s codeeeeeeee");
 
 	}
 
